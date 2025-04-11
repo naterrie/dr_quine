369bd69f9c092a89c75d3b58a985fb1362446ded
naterrie <nathanterrier38580@gmail.com>
2025-04-11 17:21:23 +0200

    adding check on printf

--- ex00/C/Colleen.c
+++ ex00/C/Colleen.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-void	print(char *s)
+int	print(char *s)
 {
-	printf(s, 10, 9, 34, s);
+	return (printf(s, 10, 9, 34, s));
 }
 
 int	main(void)
 {
 	// Comment 1
 	char *s;
-	s = "#include <stdio.h>%1$c%1$cvoid%2$cprint(char *s)%1$c{%1$c%2$cprintf(s, 10, 9, 34, s);%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// Comment 1%1$c%2$cchar *s;%1$c%2$cs = %3$c%4$s%3$c;%1$c%2$cprint(s);%1$c%2$creturn (0);%1$c}%1$c// Comment 2%1$c";
-	print(s);
+	s = "#include <stdio.h>%1$c%1$cint%2$cprint(char *s)%1$c{%1$c%2$creturn (printf(s, 10, 9, 34, s));%1$c}%1$c%1$cint%2$cmain(void)%1$c{%1$c%2$c// Comment 1%1$c%2$cchar *s;%1$c%2$cs = %3$c%4$s%3$c;%1$c%2$cif (print(s) != 0)%1$c%2$c%2$creturn (1);%1$c%2$creturn (0);%1$c}%1$c// Comment 2%1$c";
+	if (print(s) != 0)
+		return (1);
 	return (0);
 }
 // Comment 2
