7650e389f4bef0944641f3e945f5cded3d281f22
naterrie <nathanterrier38580@gmail.com>
2025-04-16 14:01:32 +0200

    adding a third define to follow correctly the subject

--- ex01/C/Grace.c
+++ ex01/C/Grace.c
@@ -2,10 +2,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define STR "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$c#define STR %2$c%3$s%2$c%1$c#define MAIN(str) int main(void){ int fd = open(%2$cGrace_kid.c%2$c, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd == -1) return (1); if (dprintf(fd, str, 10, 34, str) < 0) { close(fd); return (1); } close(fd); return (0); }%1$c%1$cMAIN(STR)%1$c/*%1$c	IM CODIIIIIIIIIIIIIIIIIIING (i guess)%1$c*/%1$c"
-#define MAIN(str) int main(void){ int fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd == -1) return (1); if (dprintf(fd, str, 10, 34, str) < 0) { close(fd); return (1); } close(fd); return (0); }
+#define STR "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$c#define STR %2$c%3$s%2$c%1$c#define FNAME %2$cGrace_kid.c%2$c%1$c#define MAIN(str, fname) int main(void){ int fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd == -1) return (1); if (dprintf(fd, str, 10, 34, str) < 0) { close(fd); return (1); } close(fd); return (0); }%1$c%1$cMAIN(STR, FNAME)%1$c/*%1$c	IM CODIIIIIIIIIIIIIIIIIIING (i guess)%1$c*/%1$c"
+#define FNAME "Grace_kid.c"
+#define MAIN(str, fname) int main(void){ int fd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (fd == -1) return (1); if (dprintf(fd, str, 10, 34, str) < 0) { close(fd); return (1); } close(fd); return (0); }
 
-MAIN(STR)
+MAIN(STR, FNAME)
 /*
 	IM CODIIIIIIIIIIIIIIIIIIING (i guess)
 */
