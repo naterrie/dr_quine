f522488b5f86e244eeda759e63235953fcd57f19
naterrie <nathanterrier38580@gmail.com>
2025-05-20 17:37:27 +0200

    adding grace.js

---
+++ ex01/JS/Grace.js
@@ -0,0 +1,13 @@
+(function grace(){
+	/*
+		JavaScript doesnt have include sorry :(
+	*/
+	const fs = require('fs')
+	const name = "Grace_kid.js"
+	const code = `(${grace.toString()})()`
+	fs.writeFile(name, code, function(error) {
+		if (error) {
+			return console.error(error)
+		}
+	})
+})()
